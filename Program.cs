452ee9d57efaf1ee89913a217aa940fd8fd30346
ernane <ernanecarvalho89@gmail.com>
2022-04-09 17:35:55 -0300

    aula de oop balta inicializado

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Balta.ContentContext;
+using Balta.ContentContext.Enums;
+
+var articles = new List<Article>();
+articles.Add(new Article("Artigo", "artigo"));
+articles.Add(new Article("tesge", "aTeste"));
+articles.Add(new Article("xpto", "xpto"));
+
+foreach(var article in articles){
+    System.Console.WriteLine(article.Id);
+    System.Console.WriteLine(article.Title);
+    System.Console.WriteLine(article.Url);
+}
